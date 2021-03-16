4ef78351106982cecae3e7680249805607bd56b7
simskiy <simskiy@gmail.com>
2021-03-16 19:08:36 +1000

    Инициализация проекта

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import './header.js' // проверка работы js
+import * as $ from 'jquery' // jquery
+import '@/styles/style.scss' //Стилиi
+
+console.log('hello, world')
---
+++ webpack.config.js
@@ -0,0 +1,147 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  devtool: isDev ? 'source-map' : false,
+	entry: {
+    // подключаем предварительно полифилл
+    main: ['@babel/polyfill', './js/index.js'],
+  },
+
+	output: {
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+	},
+
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '#': path.resolve(__dirname, './src/blocks')
+    }
+  },
+
+  optimization: {
+    minimize: isProd,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+        sourceMap: isDev
+      }),
+      new TerserPlugin()
+    ],
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
+
+  devServer: {
+    port: 8080,
+    contentBase: './dist',
+    open: true,
+    hot: isDev,
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './pages/index.pug'
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/favicon.ico'),
+          to: path.resolve(__dirname, 'dist/images')
+        },
+    //     {
+    //       from: path.resolve(__dirname, '/\.src/assets/fonts/'),
+    //       to: path.resolve(__dirname, 'dist')
+    //     }
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"],
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          "sass-loader"
+          ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+          ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: './images'
+        }
+
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: './fonts'
+        }
+
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      }
+    ],
+  }
+}

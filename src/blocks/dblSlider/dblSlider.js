1fc101f40b2f2a4ce8628acd6a5cade8c63fe8cf
simskiy <simskiy@gmail.com>
2021-03-23 23:48:58 +1000

    Компонента dblSlider (двойной ползунок). Готово.

--- src/blocks/dblSlider/dblSlider.js
+++ src/blocks/dblSlider/dblSlider.js
@@ -1,12 +1,28 @@
 import JSR from 'mm-jsr'
 
-const {a, b, min, max} = $('.dblSlider').data()
+const {a, b, min, max, step} = $('.dblSlider').data()
 
 
-new JSR(['#jsr-1-1', '#jsr-1-2'], {
+const range = new JSR(['#jsr-1-1', '#jsr-1-2'], {
     sliders: 2,
     min: min,
     max: max,
     values: [a, b],
-    grid: false
+    grid: false,
+    step: step,
+    modules: {
+        labels: false
+    }
+});
+
+function formatString(str) {
+  return str.toLocaleString('ru') + '₽'
+}
+
+$('#dblSlider__label-1').html(formatString(a))
+$('#dblSlider__label-2').html(formatString(b))
+
+range.addEventListener('update', (input, value) => {
+  if (input.id == 'jsr-1-1') $('#dblSlider__label-1').html(formatString(value))
+  if (input.id == 'jsr-1-2') $('#dblSlider__label-2').html(formatString(value))
 });

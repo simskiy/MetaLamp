589b3c53bce2a2d630f473a28047f44f0b16e355
simskiy <simskiy@gmail.com>
2021-04-19 00:14:05 +1000

    Реализация карточки товара (комопонент). Готово.

--- src/blocks/room/room.js
+++ src/blocks/room/room.js
@@ -4,5 +4,12 @@ import Rate from '@/js/lib/rate.js';
 let cardSlider1 = new Slider('#slider-1')
 cardSlider1.init()
 
-let cardRate1 = new Rate('#cardRate-1', {})
+let cardRate1 = new Rate('#cardRate-1', {
+  edit: false,
+})
 cardRate1.init()
+
+let cardRate2 = new Rate('#cardRate-2', {
+  edit: false
+})
+cardRate2.init()

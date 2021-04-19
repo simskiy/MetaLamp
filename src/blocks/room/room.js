c5696a67a06056e7fe27e844bcc36e3b95caf6f0
simskiy <simskiy@gmail.com>
2021-04-19 14:58:35 +1000

    страница cards. Готово

--- src/blocks/room/room.js
+++ src/blocks/room/room.js
@@ -4,6 +4,9 @@ import Rate from '@/js/lib/rate.js';
 let cardSlider1 = new Slider('#slider-1')
 cardSlider1.init()
 
+let cardSlider2 = new Slider('#slider-2')
+cardSlider2.init()
+
 let cardRate1 = new Rate('#cardRate-1', {
   edit: false,
 })

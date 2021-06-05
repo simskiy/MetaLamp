52ee5143a4669ac2ce780411ec014d0e6e5a2655
simskiy <simskiy@gmail.com>
2021-06-05 21:15:11 +1000

    Мелки исправления в проекте. Первичаная разметка сайдбара на странице search (подбор комнат по фильтру)

--- src/blocks/checkboxList/checkboxList.js
+++ src/blocks/checkboxList/checkboxList.js
@@ -1,9 +1,18 @@
 $('.checkboxList__title[data-expand]').click(function () {
   let attr = $(this).attr('data-expand');
-  if (attr == 'on') {
-    $(this).attr('data-expand', 'off')
-  } else {
-    $(this).attr('data-expand', 'on')
+  // if (attr == 'on') {
+  //   $(this).attr('data-expand', 'off')
+  // }
+  // if (attr == 'off') {
+  //   $(this).attr('data-expand', 'on')
+  // }
+  switch(attr) {
+    case 'on':
+      $(this).attr('data-expand', 'off')
+      break
+    case 'off':
+      $(this).attr('data-expand', 'on')
+      break
   }
   $(this).next().slideToggle();
 })
--- src/blocks/search/search.js
+++ src/blocks/search/search.js
@@ -1,4 +1,25 @@
 import '@/js/header.js'
 import '@blocks/mainNav/mainNav.js'
+import '@blocks/dblSlider/dblSlider.js'
+import '@blocks/checkboxList/checkboxList.js'
 
 require('@blocks/search/search.scss')
+
+import DropDown from '@/js/lib/dropDown.js';
+
+let filterGuest = new DropDown('#filterDropGuest', {
+  labels: [['взрослые', 0], ['дети', 0], ['младенцы', 0]],
+  minimize: true,
+  preset: 'guest',
+  title: 'Гости'
+})
+
+let filterComfort = new DropDown('#filterDropComfort', {
+  labels: [['спальни', 2], ['кровати', 2], ['ванные комнаты', 0]],
+  minimize: true,
+  preset: 'room',
+  showBtns: false
+})
+
+filterGuest.init()
+filterComfort.init()

45050ea84042e778af863ee5c4df2b8e303a9a14
simskiy <simskiy@gmail.com>
2021-06-06 11:32:42 +1000

    Добавил карточки на страницу search. Первичная разметка (css grid). Создал компонент header. Перенес главную навигацию в header.

--- src/blocks/search/search.js
+++ src/blocks/search/search.js
@@ -2,10 +2,13 @@ import '@/js/header.js'
 import '@blocks/mainNav/mainNav.js'
 import '@blocks/dblSlider/dblSlider.js'
 import '@blocks/checkboxList/checkboxList.js'
+import '@blocks/pagination/pagination.js'
 
 require('@blocks/search/search.scss')
 
-import DropDown from '@/js/lib/dropDown.js';
+import DropDown from '@/js/lib/dropDown.js'
+import Slider from '@/js/lib/slider.js'
+import Rate from '@/js/lib/rate.js'
 
 let filterGuest = new DropDown('#filterDropGuest', {
   labels: [['взрослые', 0], ['дети', 0], ['младенцы', 0]],
@@ -23,3 +26,11 @@ let filterComfort = new DropDown('#filterDropComfort', {
 
 filterGuest.init()
 filterComfort.init()
+
+let rooms = document.querySelectorAll('.rooms__item')
+for (let i = 1; i < rooms.length + 1; i++) {
+  new Slider(`#slider-${i}`).init()
+  new Rate(`#cardRate-${i}`, {
+    edit: false
+  }).init()
+}
